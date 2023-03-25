e6e1d20dd37f612b97d194f5b10df9981a21eaec
miyagemonorgb <miyagemono.rgbcolor@gmail.com>
2023-03-25 17:43:21 +0900

    kusamaフォルダ内ファイルのコミット

---
+++ kusama/js/pumpkin.js
@@ -0,0 +1,7 @@
+
+window.onload = function init() {
+  let Height = window.innerHeight;
+  let Width = window.innerWidth;
+  document.getElementById('wrap').style.height = Height * 2 + 'px';
+  document.getElementById('wrap').style.width = Width + 'px';
+}
